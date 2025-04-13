dc855d36a740b9c8dc53ab1d3c716b0b9682769d
simlx <simlx@mailbox.org>
2025-04-13 11:50:42 +0200

    generate tiles based on world map

--- src/main.c
+++ src/main.c
@@ -6,16 +6,19 @@
 
 #define GRAVITY 1
 #define PLAYER_SPEED 1
+#define JETPACK_SPEED 1.75f
 #define TILES_NUM 600
 
 #define GAME_SCALE 3
+
+//#define DEBUG_PLAYER_COLLISIONS
 
 #define _ 0
 
 // TYPES >>
 typedef struct {
     Texture2D *img;
-    Rectangle rect; // General Rectangle
+    Rectangle rect; // Base Rectangle
 } ent;
 typedef struct {
     ent base;
@@ -74,8 +77,12 @@ void register_input(m_ent *ply)
 
     if (IsKeyDown(KEY_UP)) {
         if (player_fuel > 0) {
-            ply->base.rect.y -= PLAYER_SPEED * 2;
-            player_fuel -= 0.1f;
+            ply->base.rect.y -= JETPACK_SPEED;
+            player_fuel -= 1.0f;
+        }
+    } else {
+        if (player_fuel < 99.9f) {
+            player_fuel += 0.25f;
         }
     }
 }
@@ -83,17 +90,29 @@ void register_input(m_ent *ply)
 void create_world(ent *tiles, Texture2D *map_tile_texture)
 {
     int x = 0;
-    int y = 5;
+    int y = 1;
     int spacing = 16;
     for(int i = 0 ; i < TILES_NUM ; i++)
     {
         x++;
+
         tiles[i].img = map_tile_texture;
-        tiles[i].rect.x = x * spacing;
-        tiles[i].rect.y = y * spacing;
-        tiles[i].rect.width = 16;
-        tiles[i].rect.height = 16;
-        if (x > 50)
+
+        // Wall
+        if (world[x + (y * 40)] == 1)
+        {
+            tiles[i].rect.x = x * spacing;
+            tiles[i].rect.y = y * spacing;
+            tiles[i].rect.width = 16;
+            tiles[i].rect.height = 16;
+        }
+        // Anything else is not visible
+        else {
+            tiles[i].rect.x = -999*spacing;
+            tiles[i].rect.y = -999*spacing;
+        }
+
+        if (x >= 40)
         {
             x = 0;
             y++;
@@ -159,10 +178,14 @@ void apply_m_ent_collision(m_ent *e, ent *tiles)
         }
     }
 
+    if (colliding_with_floor) {
+        colliding_with_top = false;
+    }
+
     apply_gravity(e, colliding_with_floor);
     e->base.rect.x += colliding_with_left ? PLAYER_SPEED : 0;
     e->base.rect.x -= colliding_with_right ? PLAYER_SPEED : 0;
-    e->base.rect.y += colliding_with_top ? PLAYER_SPEED : 0;
+    e->base.rect.y += colliding_with_top ? JETPACK_SPEED*0.5 : 0;
 }
 // Load a texture from a file and replace MAGENTA with BLANK
 Texture2D load_texture(const char *path)
@@ -176,21 +199,27 @@ Texture2D load_texture(const char *path)
 
 void update_m_ent(m_ent *e, ent *tiles)
 {
-    e->right_collision_rect.x = e->base.rect.x + 14;
-    e->right_collision_rect.y = e->base.rect.y + 13;
+    e->right_collision_rect.x = e->base.rect.x + 12;
+    e->right_collision_rect.y = e->base.rect.y + 3;
 
-    e->left_collision_rect.x = e->base.rect.x ;
-    e->left_collision_rect.y = e->base.rect.y + 13;
+    e->left_collision_rect.x = e->base.rect.x + 0;
+    e->left_collision_rect.y = e->base.rect.y + 3;
 
-    e->down_collision_rect.x = e->base.rect.x;
+    e->down_collision_rect.x = e->base.rect.x + 1;
     e->down_collision_rect.y = e->base.rect.y + 14;
+
+    e->top_collision_rect.x = e->base.rect.x + 1;
+    e->top_collision_rect.y = e->base.rect.y + 1;
+
+
+
 }
 
 int main(void)
 {
     // INIT >>
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "mygame");
-	SetTargetFPS(30);
+	SetTargetFPS(60);
     // INIT <<
 	
     Texture2D player_texture = load_texture("res/player_base.png");
@@ -199,10 +228,10 @@ int main(void)
 
     m_ent ply = { 
         (ent) {&player_texture, {20.0f,20.0f,16,16}},
-        {20.0f,20.0f,2,2},
-        {20.0f,20.0f,2,2},
-        {20.0f,20.0f,14,2},
-        {20.0f,20.0f,14,2},
+        {20.0f,20.0f,2,10},
+        {20.0f,20.0f,2,10},
+        {20.0f,20.0f,12,2},
+        {20.0f,20.0f,12,2},
         &player_invert_texture,
         false
     };
@@ -247,10 +276,19 @@ int main(void)
             draw_ent(ply.inverted_img, &ply.base.rect);
 
         draw_world(tiles);
-    
+ 
+        #ifdef DEBUG_PLAYER_COLLISIONS   
+            Color debug_clr = (Color) {255,0,255,200};
+            DrawRectangleRec(ply.left_collision_rect, debug_clr);
+            DrawRectangleRec(ply.right_collision_rect, debug_clr);
+            DrawRectangleRec(ply.down_collision_rect, debug_clr);
+            DrawRectangleRec(ply.top_collision_rect, debug_clr);
+        #endif
+
         EndMode2D();
 
         draw_jetpack_meter();
+
 
         EndDrawing();
         // RENDER <<
