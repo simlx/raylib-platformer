91df04e76a77a08e3c3e910d8d9c46afe266c520
simlx <simlx@mailbox.org>
2026-02-22 00:10:51 +0100

    Optimize executable size

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 @echo off
-gcc src/main.c -Iinclude -Llib -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows -Os -s -o mygame.exe
+gcc src/main.c -Iinclude -Llib -lraylib -lopengl32 -lgdi32 -lwinmm -mwindows -Os -s -o jjump.exe
+upx.exe jjump.exe
